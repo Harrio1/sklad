41e5cb4ae5db02e1b0f0a30e6a669c2dbe9a8ebe
Sarkis8321 <danielyan.sar@yandex.ru>
2024-09-27 11:41:28 +0300

    Добавил форму для products-nomenclatures связи

--- app/Http/Controllers/ProductsNomenclatureController.php
+++ app/Http/Controllers/ProductsNomenclatureController.php
@@ -5,23 +5,44 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Products;
+use App\Models\Nomenclatures;
+use App\Models\Products_Nomenclature;
 
 class ProductsNomenclatureController extends Controller
 {
     function getProductById(Request $request) {
         $id = $request->id;
 
-        
         $product = Products::find($id);
-        
-      
-
+        $nomenclatures = Nomenclatures::all();
+        //$productNomenclatures = $product->nomenclatures;
         if (!$product) {
             return Inertia::render('Products');
         } else {
-            return Inertia::render('ProductsNomenclatures', ['product' => $product ]);
+            return Inertia::render('ProductsNomenclatures', ['product' => $product, 'nomenclatures' => $nomenclatures ]);
         }
-
-        
     }
+
+    function addNewProductNomenclature(Request $request) {
+
+        $validated = $request->validate([
+            'product' => ['required', 'numeric'],
+            'nomenclature' => ['required','numeric'],
+            'count' => ['required','numeric'],
+        ]);
+        
+           if ($validated){
+                $product = new Products_Nomenclature;
+                $product->product_id = $request->product;
+                $product->nomenclature_id = $request->nomenclature;
+                $product->quantity = $request->count;
+                $product->save();
+                return Response::json(['status' => 'Данные добавлены', 'isOk' => True], 200);
+           } else {
+                return Response::json(['status' => 'Ошибка формата входных данных', 'isOk' => False], 200);
+           }
+
+
+    }
+
 }
--- resources/js/Pages/ProductsNomenclatures.vue
+++ resources/js/Pages/ProductsNomenclatures.vue
@@ -8,10 +8,26 @@ import { Link } from '@inertiajs/vue3';
 
 let csrf = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 
-
-defineProps({
+const { product, nomenclatures } = defineProps({
     product: Object,
- });
+    nomenclatures: Object,
+});
+
+const form = reactive({
+    product: product.id,
+    nomenclature: null,
+    count: null,
+});
+
+
+
+
+
+function addNomenclature(){
+    console.log(form);
+
+    // отправить
+}
 
 </script>
 
@@ -22,16 +38,37 @@ defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
                     
-                    
-                  
-                    <h3 class="text-lg font-medium mb-4">Добавить новый продукт</h3>
+                    <h3 class="text-lg font-medium mb-4">{{ product.name }}</h3>
+                    {{ product.price }}
 
-        <div v-for="item in product" :key="item">
-            {{ item }}
-        </div>
+                    <hr>
 
-        </div>
-        </div>
+                    <h3 class="text-lg font-medium mb-4">Добавить номенклатуру к продукту</h3>
+                    <form @submit.prevent>
+                        <div class="mb-4">
+                            <label for="nomenclature" class="block text-sm font-medium text-gray-700">Номенклатура</label>
+                            <select id="nomenclature" v-model="form.nomenclature" required
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500 focus:border-blue-500">
+                                <option v-for="item in nomenclatures" :key="item.id" :value="item.id">{{ item.name }}</option>
+                            </select>
+                            <div class="mb-4">
+                                <label for="nomen_count" class="block text-sm font-medium text-gray-700">Количество</label>
+                                <input type="text" id="nomen_count" v-model="form.count" required
+                                    class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500 focus:border-blue-500" />
+                            </div>
+                            <button type="submit" @click.prevent ="addNomenclature()"
+                                class="inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                Добавить номенклатуру
+                            </button>
+                        </div>
+
+
+
+                    </form>
+
+
+                </div>
+            </div>
         </div>
 
 
